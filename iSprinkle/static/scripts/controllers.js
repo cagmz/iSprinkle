51f0c877fc8d643e5d0d02b4805eedbe3876a8d4
Carlos Adrian Gomez <cagomez805@gmail.com>
2016-08-06 16:17:18 -0600

    ScheduleTable injection compiles and binds

--- iSprinkle/static/scripts/controllers.js
+++ iSprinkle/static/scripts/controllers.js
@@ -5,45 +5,61 @@ iSprinkleApp.controller('HomeController',
     $scope.helloWorld = "HomeController";
   });
 
-iSprinkleApp.controller('ScheduleController', ['$scope', '$http', '$log', 'StationFactory',
-  function ScheduleController($scope, $http, $log, StationFactory) {
+
+iSprinkleApp.controller('ScheduleController', ['$scope', '$http', '$log', '$compile','StationFactory',
+  function ScheduleController($scope, $http, $log, $compile ,StationFactory) {
 
     $scope.numberOfStations = -1;
     StationFactory.getNumberOfStations().then(function (response) {
       $scope.numberOfStations = response.data.stations;
       StationFactory.getSchedule().then(function (response) {
 
-        $scope.schedule = response.data;
-
-        if ($scope.numberOfStations && $scope.schedule) {
-          $log.debug($scope.numberOfStations);
-          $log.debug($scope.schedule);
-        }
+        $scope.scheduleData = response.data;
+        $log.debug($scope.scheduleData);
 
         $scope.weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        // view uses tableHeader to create table heading
         $scope.tableHeader = ['Stations'].concat($scope.weekdays);
 
-        $scope.cellInject = new Array($scope.numberOfStations);
-        var l = 0, m = 0;
-        for (var i = 0; i < $scope.numberOfStations; i++) {
-          $scope.cellInject[i] = "<tr><td>Station " + i + "</td>";
-          for (var j = 0; j < $scope.weekdays.length; j++, l++, m++) {
-            var startTime = ['<label for="s' + l + 'startTime">Start:</label>',
-              '            <input id="s' + l + '_startTime" type="text" class="input-small">',
-              '            <script type="text/javascript">',
-              '            $("#s' + l + '_startTime").timepicker({',
-              '                defaultTime:\'12:00 AM\',',
-              '                template: false,',
-              '                showInputs: false,',
-              '                minuteStep: 5',
-              '            });',
-              '        </script>',
-              ''
+        var rowInject = '<thead><tr><th ng-repeat="column in tableHeader">{{ column }}</th></tr></thead><tbody>';
+        for (var station in $scope.scheduleData.schedule) {
+          $log.debug(station);
+          // create a copy of weekdays array from index 0
+          var tempWeekdays = $scope.weekdays.slice(0);
+          // for each station (row), iterate through the weekdays and populate the cells
+          // each station may have a set time and duration
+          var stationSchedule = $scope.scheduleData.schedule[station];
+          var stationNumber = station.substring(1);
+          rowInject += "<tr><td>Station " + stationNumber + "</td>";
+          // debugger;
+          for(var i = 0; i < $scope.weekdays.length; i++) {
+            // dayInfo is an object with start time (string) and duration (int)
+            // eg 'Monday' {"start_time": "12:00 AM", "duration": 5}
+            //debugger;
+            var currentDay = $scope.weekdays[i];
+            var dayInfo = stationSchedule[currentDay];
+            var startTime = dayInfo['start_time'];
+            var duration = dayInfo['duration'];
+
+            var startTime = ['<label for="'+ station + '_startTime">Start:</label>',
+              //'            <input id="'+ station + '_startTime" type="text" class="input-small">',
+              '            <input id="'+ station + '_startTime" ng-model="scheduleData.schedule.' + station+ '.' + currentDay+ '.' + 'start_time" type="text" class="input-small">',
+              //'            <script type="text/javascript">',
+              //'            $("#' + station + '_startTime").timepicker({',
+              //'                defaultTime:\'' + dayInfo['start_time'] + '\',',
+              //'defaultTime: false,',
+              //'                template: false,',
+              //'                showInputs: false,',
+              //'                minuteStep: 5',
+              //'            });',
+              //'        </script>',
+              //''
             ].join('');
-            var duration = ['<label for="s' + l + '_duration">Time:</label>',
-              '            <input id="s' + m + '_duration" type="text" class="input-small" placeholder="HH:MM">',
+            var duration = ['<label for="'+ station + '_duration">Time:</label>',
+              '            <input id="'+ station + '_duration" type="text" class="input-small" placeholder="HH:MM">',
               '            <script type="text/javascript">',
-              '            $("#s' + m + '_duration").timepicker({',
+              '            $("#' + station + '_duration").timepicker({',
+              // '                defaultTime:' + dayInfo['duration'] +',',
               '                defaultTime:0,',
               '                template: false,',
               '                showMeridian: false,',
@@ -53,12 +69,15 @@ iSprinkleApp.controller('ScheduleController', ['$scope', '$http', '$log', 'Stati
               '        </script>',
               ''
             ].join('');
-            $scope.cellInject[i] += "<td>" + startTime + duration + "</td>";
+            rowInject += "<td>" + startTime + duration + "</td>";
           }
-          $scope.cellInject[i] += "</tr>"
-          $('#scheduleTable > tbody').append($scope.cellInject[i]);
+          rowInject += "</tr>";
+          //debugger;
         }
-
+        rowInject += "</tbody>";
+        $log.debug(rowInject);
+        // must call compile so that Angular binds the injected Javscript with the DOM
+        $('#scheduleTable').append($compile(rowInject)($scope));
       });
     });
 
