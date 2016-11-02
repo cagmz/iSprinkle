9e96b36f024c782b9152380b0b0ddd655239463a
Carlos Adrian Gomez <cagomez805@gmail.com>
2016-11-01 17:22:37 -0700

    Created Manual page

--- iSprinkle/static/scripts/app.js
+++ iSprinkle/static/scripts/app.js
@@ -1,28 +1,32 @@
 var iSprinkleApp = angular.module('iSprinkleApp', ['ngRoute', 'ngSanitize', 'nvd3']);
 
-iSprinkleApp.config(['$routeProvider', '$logProvider', function($routeProvider, $logProvider) {
-  // for debugging
-  $logProvider.debugEnabled(true);
+iSprinkleApp.config(['$routeProvider', '$logProvider', function ($routeProvider, $logProvider) {
+    // for debugging
+    $logProvider.debugEnabled(true);
 
-  // routes for controllers
-  $routeProvider
-  .when('/', {
-    templateUrl: 'static/views/home/index.html',
-    controller: 'HomeController'
-  })
-  .when('/home', {
-    templateUrl: 'static/views/home/index.html',
-    controller: 'HomeController'
-  })
-  .when('/schedule', {
-    templateUrl: 'static/views/schedule/index.html',
-    controller: 'ScheduleController'
-  })
-  .when('/admin', {
-    templateUrl: 'static/views/admin/index.html',
-    controller: 'AdminController'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+    // routes for controllers
+    $routeProvider
+        .when('/', {
+            templateUrl: 'static/views/home/index.html',
+            controller: 'HomeController'
+        })
+        .when('/home', {
+            templateUrl: 'static/views/home/index.html',
+            controller: 'HomeController'
+        })
+        .when('/manual', {
+            templateUrl: 'static/views/manual/index.html',
+            controller: 'ManualController'
+        })
+        .when('/schedule', {
+            templateUrl: 'static/views/schedule/index.html',
+            controller: 'ScheduleController'
+        })
+        .when('/admin', {
+            templateUrl: 'static/views/admin/index.html',
+            controller: 'AdminController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }]);
