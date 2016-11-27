47670c470b9656a845550d29deff228e3b110452
Carlos Adrian Gomez <cagomez805@gmail.com>
2016-11-26 18:11:49 -0800

    Fetches timezone from RPi system config. Build now works on RPi (GPIO not implemented yet)

--- iSprinkle/StationControl.py
+++ iSprinkle/StationControl.py
@@ -1,8 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timezone
 from dateutil.parser import parse
 from dateutil.tz import tz
 from arrow import Arrow
-import datetime
 import atexit
 
 GPIO = None
@@ -25,13 +25,12 @@ class StationControl(object):
         self.data_handler = data_handler
         self.watering = False
 
-        # schedule dict is a parsed version of the watering schedule in settings.json
-        # unparsed schedule is in SettingsHandler object
+        # self.data_handler.get_schedule()['timezone_offset']
+        self.utc_timezone_offset = datetime.now(timezone.utc).astimezone().strftime('%z')
+        self.timezone_name = datetime.now(timezone.utc).astimezone().tzname()
+        print('Operating in {} timezone ({})'.format(self.timezone_name, self.utc_timezone_offset))
         self.bg_scheduler = BackgroundScheduler()
-        self.manual_scheduler = None
         self.set_schedule(self.data_handler.get_schedule())
-        self.utc_timezone_offset = self.data_handler.get_schedule()['timezone_offset']
-        print(self.utc_timezone_offset)
 
         if GPIO:
             atexit.register(self.cleanup)
@@ -39,32 +38,31 @@ class StationControl(object):
             self.setup_gpio()
 
     def set_schedule(self, settings_json):
-        print('Setting schedule at {}'.format(Arrow.now().time().isoformat()))
-        # schedule dictionary:
-        # (key) = (value) => (station, day) = (datetime in utc, watering duration)
-
         self.bg_scheduler.remove_all_jobs()
 
         stations = settings_json['schedule']
-        timezone_offset = settings_json['timezone_offset']
+        timezone_offset = self.utc_timezone_offset #settings_json['timezone_offset']
         for station in stations:
             for day in stations[station]:
                 for start_time in stations[station][day]['start_times']:
+                    station_id = int(station[-1])
                     time_str = day + " " + start_time['time'] + " " + timezone_offset
                     # get 12 hour time and convert to time-zone aware datetime object (24 hour UTC time) for use internally
                     utc_time = timestr_to_utc(time_str)
                     duration = int(start_time['duration'])
-                    # todo: set 'interval' trigger instead of 'date'.
-                    # this way, manual_watering() doesn't need to remove/replace all jobs
-                    self.bg_scheduler.add_job(self.water, 'date', run_date=utc_time, args=[station, time_str, duration])
+                    print('utc time is {}'.format(utc_time))
+                    print('Station {} will start at {} for {} minutes'.format(station_id, utc_time, duration))
+                    # TODO: set 'interval' trigger instead of 'date'.
+                    # Jobs may exit scheduler after running once if the 'date' trigger is used
+                    self.bg_scheduler.add_job(self.water, 'date', run_date=utc_time, args=[station_id, time_str, duration])
 
-            print('Added water_schedule jobs for {}'.format(station))
+            print('Added start times for station {}'.format(station))
         # start the scheduler if it's not already running
         if not self.bg_scheduler.state:
             self.bg_scheduler.start()
 
     def pause_schedule(self):
-        print('Pausing schedule')
+        print('Pausing schedule...')
         jobs_paused = 0
         for job in self.bg_scheduler.get_jobs():
             job.pause()
@@ -73,7 +71,7 @@ class StationControl(object):
         return jobs_paused
 
     def resume_schedule(self):
-        print('Resuming schedule')
+        print('Resuming schedule...')
         for job in self.bg_scheduler.get_jobs():
             job.resume()
             print('Resumed job {}'.format(job))
@@ -135,22 +133,21 @@ class StationControl(object):
             GPIO.output(StationControl.out_pin, True)
 
     def setup_gpio(self):
+        
         if not GPIO:
             print('Error: setup_gpio() doesn\'t have GPIO module')
             return
-
-        print('Setting up GPIO')
-        # clean up after any previous applications
-        GPIO.cleanup()
+       
         # setup GPIO pins to interface with shift register
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(StationControl.clock_pin, GPIO.OUT)
         GPIO.setup(StationControl.out_pin, GPIO.OUT)
-        self.toggleShiftRegisterOutput(False)
+        self.toggle_shift_register_output(False)
         GPIO.setup(StationControl.data_pin, GPIO.OUT)
         GPIO.setup(StationControl.latch_pin, GPIO.OUT)
-        self.setShiftRegisterValues()
-        self.toggleShiftRegisterOutput(True)
+        self.set_shift_register_values()
+        self.toggle_shift_register_output(True)
+        print('GPIO setup successfully')
 
     def cleanup(self):
         self.reset_stations()
@@ -161,7 +158,7 @@ class StationControl(object):
         import time
         print(
             'water(station={}, scheduled_time={}, duration={}), time_now = {}'.format(station, scheduled_time, duration,
-                                                                                      str(datetime.datetime.now())))
+                                                                                      str(datetime.now())))
 
         # activate solenoid
         seconds = int(duration) * 60
